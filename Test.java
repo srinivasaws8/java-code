372a2b59e605dcb5b2fd845f841541dbbd4b9a86
srinivasaws8 <90260994+srinivasaws8@users.noreply.github.com>
2024-06-25 14:42:42 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("**************************************");
-		System.out.println("Hello from GITHub");
+		System.out.println("Hello from Srini GITHub");
 		System.out.println("**************************************");
 	}
 
